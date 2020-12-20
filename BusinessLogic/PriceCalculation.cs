3f042cbd1e88a0b2f6774ab7f9c49134e17d62eb
Siddhartha Chakraborty <Siddhartha_Chakrabo1@Dellteam.com>
2020-12-20 23:56:27 +0530

    added business logic and interfaces to do DI

---
+++ BusinessLogic/PriceCalculation.cs
@@ -0,0 +1,15 @@
+﻿using PromotionEngine.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.BusinessLogic
+{
+    public interface IPriceCalculation
+    {
+        double GetTotalPrice(List<Sku> skus);
+    }
+   public class PriceCalculation
+    {
+    }
+}
